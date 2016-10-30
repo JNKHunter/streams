eb1684e6b05cb252a47b1e2ae0b701bad8968cce
John Hunter <john@pushagency.io>
2016-10-30 10:15:25 -0700

    foreach stream and gitignore

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 /**
  * Created by John on 10/29/16.
@@ -13,5 +14,22 @@ public class Main {
         models.add(new Car(Colors.PURPLE));
         models.add(new Car(Colors.RED));
         models.add(new Car(Colors.YELLOW));
+
+        //List of terminal operations in the Stream<T> interface
+        //forEach, forEachOrdered, toArray, reduce, collect, min, max, count,
+
+        //Perform the forEach terminal operation on the stream
+        System.out.println("_______________________________________");
+        System.out.println("For each terminal operation");
+        System.out.println("...");
+        models.stream().forEach(car -> System.out.println(car.getColor()));
+        System.out.println("_______________________________________");
+
+        //Perform the
+
+        Stream<Car> filteredStream = models.stream().filter(car -> car.getColor() == Colors.BLUE);
+        filteredStream.forEach(car -> System.out.println(car.getColor()));
+
+
     }
 }
